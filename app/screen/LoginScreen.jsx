a770802d91fa52a8441c1e9710ed7662e5c48cbc
AnushkaPraveen <anushkapraveen98@gmail.com>
2024-02-19 12:26:44 +0530

    new screens add

--- app/screen/LoginScreen.jsx
+++ app/screen/LoginScreen.jsx
@@ -1,8 +1,36 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
+import * as WebBrowser from "expo-web-browser";
+import { useOAuth } from "@clerk/clerk-expo";
 import Colors from "../utils/Colors";
+import { useWarmUpBrowser } from "../../hooks/useWarmUpBrowser";
+
+WebBrowser.maybeCompleteAuthSession();
+
+
+
+
+
 
 export default function LoginScreen() {
+useWarmUpBrowser();
+const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+
+ const onPress = async () => {
+  try {
+    const { createdSessionId, signIn, signUp, setActive } =
+      await startOAuthFlow();
+
+    if (createdSessionId) {
+      setActive({ session: createdSessionId });
+    } else {
+      // Use signIn or signUp for next steps such as MFA
+    }
+  } catch (err) {
+    console.error("OAuth error", err);
+  }
+}; 
+
   return (
     <View style={styles.container}>
       <Image
@@ -15,13 +43,13 @@ export default function LoginScreen() {
       />
       <View style={styles.headingContainer}>
         <Text style={styles.headingText}>
-          Your Ulimate EV Charging Station Finder App
+          Your Ultimate EV Charging Station Finder App
         </Text>
         <Text style={styles.description}>
           Find EV charging station near you, plan trip and so much move in just
           one click
         </Text>
-        <TouchableOpacity style={styles.button} onPress={()=>console.log("Clicked")}>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
           <Text style={styles.buttonText}>Login With Google</Text>
         </TouchableOpacity>
       </View>

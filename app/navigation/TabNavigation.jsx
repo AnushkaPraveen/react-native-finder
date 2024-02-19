ca5984fa81b4bca295153e0a97b2201219faa43c
AnushkaPraveen <anushkapraveen98@gmail.com>
2024-02-19 12:26:29 +0530

    tab navigation screen add

---
+++ app/navigation/TabNavigation.jsx
@@ -0,0 +1,38 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import HomeScreen from '../screen/HomeScreen';
+import FavoriteScreen from '../screen/FavoriteScreen';
+import ProfileScreen from '../screen/ProfileScreen';
+import Colors from '../utils/Colors';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigation() {
+  return (
+    <Tab.Navigator screenOptions={{
+        headerShown:false
+    }}>
+      <Tab.Screen name='home' component={HomeScreen} options={{
+        tabBarLabel:'Search',
+        tabBarActiveTintColor:Colors.PRIMARY,
+        tabBarIcon:({color,size})=>(
+        <Ionicons name="search" size={size} color={color} />)
+      }}/>
+      <Tab.Screen name='favorite' component={FavoriteScreen}  options={{
+        tabBarLabel:'Favorite',
+        tabBarActiveTintColor:Colors.PRIMARY,
+        tabBarIcon:({color,size})=>(
+        <Ionicons name="heart" size={size} color={color} />)
+      }}/>
+      <Tab.Screen name='profile' component={ProfileScreen}  options={{
+        tabBarLabel:'Profile',
+        tabBarActiveTintColor:Colors.PRIMARY,
+        tabBarIcon:({color,size})=>(
+        <FontAwesome name="user" size={size} color={color} />)
+      }}/>
+    </Tab.Navigator>
+  )
+}
